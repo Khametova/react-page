c262a79080fd034f6b1413f4786ffe4462d95c1c
Alina Khametova <alinakhametova1@gmail.com>
2026-04-30 16:04:00 +0300

    add 'Fetching'

--- src/Pages/UsersPage/UsersList/index.jsx
+++ src/Pages/UsersPage/UsersList/index.jsx
@@ -2,18 +2,33 @@ import { useEffect, useState } from "react";
 
 function UsersList() {
   const [users, setUsers] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setIsFetching(true);
+    setError(null);
     fetch("https://randomuser.me/api/?results=10")
       .then((response) => response.json())
       .then(({ results }) => setUsers(results))
-      .catch((e) => console.log("e", e));
-  });
+      .catch((e) => setError(e))
+      .finally(() => setIsFetching(false));
+  }, []);
   return (
     <ul>
-      {users.map((u) => (
-        <li>{JSON.stringify(u)}</li>
-      ))}
+      {error && <div> ERROR !!!!</div>}
+      {isFetching && <div> Data ioading ....Please wait</div>}
+      {!error &&
+        !isFetching &&
+        users.map((u) => (
+          <li key={u.login.uuid}>
+            {Object.keys(u).map((key) => (
+              <p key={key}>
+                {key}: {JSON.stringify(u[key])}
+              </p>
+            ))}
+          </li>
+        ))}
     </ul>
   );
 }
