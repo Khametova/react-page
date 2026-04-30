36d5872fbb638c12d53951afc7bf6a0fa72ab8f6
Alina Khametova <alinakhametova1@gmail.com>
2026-04-30 18:25:09 +0300

    add  "icons on btn"

--- src/Pages/UsersPage/UsersList/index.jsx
+++ src/Pages/UsersPage/UsersList/index.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { DNA } from "react-loader-spinner";
+import { FcNext } from "react-icons/fc";
+import { FcPrevious } from "react-icons/fc";
 
 function UsersList() {
   const [users, setUsers] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [results, setResults] = useState(10);
 
   useEffect(() => {
     setIsFetching(true);
@@ -16,32 +20,42 @@ function UsersList() {
       .finally(() => setIsFetching(false));
   }, []);
   return (
-    <ul>
-      {error && <div> ERROR !!!!</div>}
-      {isFetching && (
-        <div>
-          <DNA
-            visible={true}
-            height="80"
-            width="100"
-            ariaLabel="dna-loading"
-            wrapperStyle={{}}
-            wrapperClass="dna-wrapper"
-          />
-        </div>
-      )}
-      {!error &&
-        !isFetching &&
-        users.map((u) => (
-          <li key={u.login.uuid}>
-            {Object.keys(u).map((key) => (
-              <p key={key}>
-                {key}: {JSON.stringify(u[key])}
-              </p>
-            ))}
-          </li>
-        ))}
-    </ul>
+    <>
+      <ul>
+        {error && <div> ERROR !!!!</div>}
+        {isFetching && (
+          <div>
+            <DNA
+              visible={true}
+              height="100"
+              width="100"
+              ariaLabel="dna-loading"
+              wrapperStyle={{}}
+              wrapperClass="dna-wrapper"
+            />
+          </div>
+        )}
+        {!error &&
+          !isFetching &&
+          users.map((u) => (
+            <li key={u.login.uuid}>
+              {Object.keys(u).map((key) => (
+                <p key={key}>
+                  {key}: {JSON.stringify(u[key])}
+                </p>
+              ))}
+            </li>
+          ))}
+      </ul>
+      <div>
+        <button>
+          <FcPrevious />
+        </button>
+        <button>
+          <FcNext />
+        </button>
+      </div>
+    </>
   );
 }
 
