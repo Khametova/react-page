92c6701663d53e2ceeb7f290909f1e0be06b1326
Alina Khametova <alinakhametova1@gmail.com>
2026-04-30 14:22:57 +0300

    add  'useEffect'

--- src/Pages/UsersPage/UsersList/index.jsx
+++ src/Pages/UsersPage/UsersList/index.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 function UsersList() {
-  return <div>UsersList</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+      .then((response) => response.json())
+      .then(({ results }) => setUsers(results))
+      .catch((e) => console.log("e", e));
+  });
+  return (
+    <ul>
+      {users.map((u) => (
+        <li>{JSON.stringify(u)}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default UsersList;
